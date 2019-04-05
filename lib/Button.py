d8792d6c354df869a4e4e579f286e2104f26a53e
hveficent <32061121+hveficent@users.noreply.github.com>
2019-04-05 16:24:03 +0200

    Revert "Improve button sensing (#35)"
    This reverts commit 2b08b67b083a7d4067367b98e7b244ca247f629d.

--- lib/Button.py
+++ lib/Button.py
@@ -18,15 +18,16 @@ class Button:
         GPIO.setup(self.pin_power, GPIO.OUT)
         GPIO.output(self.pin_power, 0)
         self.pressed = False
-        GPIO.add_event_detect(self.pin_signal, GPIO.FALLING,
-                              callback=self.scanning,
-                              bouncetime=400)
         _logger.debug('Button Class Initialized')
 
-    def scanning(self, channel):
+    def scanning(self):
         GPIO.output(self.pin_power, 1)
-        self.pressed = True
-        _logger.debug('Button Pressed')
+        if not self.pressed and GPIO.input(self.pin_signal) == GPIO.HIGH:
+            self.pressed = True
+            _logger.debug('Button Pressed')
+        elif self.pressed and GPIO.input(self.pin_signal) == GPIO.LOW:
+            self.pressed = False
+            _logger.debug('Button Unpressed')
         time.sleep(0.02)
 
     def poweroff(self):
