40f4dc18f82fd7dad47d3dcf43d29616c3b5201f
Pablo Fuentes <pablo@studio73.es>
2019-07-03 17:18:51 +0200

    [FIX] Push buttons

--- lib/Button.py
+++ lib/Button.py
@@ -12,28 +12,30 @@ class Button:
         GPIO.setmode(GPIO.BOARD)  # Sets GPIO pins to Board GPIO numbering
         self.pin_signal = pins[0]
         self.pin_power = pins[1]
-        GPIO.setup(self.pin_signal, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        self.active = True
+        GPIO.setup(self.pin_signal, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.pin_power, GPIO.OUT)
         GPIO.output(self.pin_power, 0)
         self.pressed = False
         GPIO.add_event_detect(
             self.pin_signal,
-            GPIO.FALLING,
+            GPIO.BOTH,
             callback=self.scanning,
-            bouncetime=400,
         )
         _logger.debug("Button Class Initialized")
 
     def scanning(self, channel):
-        GPIO.output(self.pin_power, 1)
-        self.pressed = True
-        _logger.debug("Button Pressed")
-        time.sleep(0.02)
+        if self.active and GPIO.input(self.pin_signal):
+            self.pressed = True
+            _logger.debug("Button Pressed")
+            time.sleep(0.02)
 
     def poweroff(self):
+        self.active = False
         GPIO.output(self.pin_power, 0)
         _logger.debug("Button Power off")
 
     def poweron(self):
+        self.active = True
         GPIO.output(self.pin_power, 1)
         _logger.debug("Button Power on")
